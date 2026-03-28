95e0b6b1670aa594bbae9ffaa0f2c128ab32e655
sujitmendhe <sujit.mendhe@gmail.com>
2026-03-28 13:42:06 +0530

    terra config5

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.${count.index}.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zone.available.names[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
   tags = {
     Name = "public-subnet-${count.index}"
   }
@@ -91,7 +91,7 @@ resource "aws_subnet" "private" {
   count             = 2
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.${count.index + 2}.0/24"
-  availability_zone = data.aws_availability_zone.available.names[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
   tags = {
     Name = "private-subnet-${count.index}"
   }
